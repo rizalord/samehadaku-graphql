63617523632ee292b6876804f8ea9456984d7d5f
rizalord <ahmadkhamdani9@gmail.com>
2020-06-08 17:41:07 +0700

    add read_blog route

--- router/routes.ts
+++ router/routes.ts
@@ -1,9 +1,11 @@
 import HomeSchema from './../schema/home_schema.ts';
 import BlogSchema from './../schema/blog_schema.ts';
+import BlogReadSchema from "./../schema/blog_read_schema.ts";
 
 export default {
     use : function(app : any){
         app.use(HomeSchema.routes(), HomeSchema.allowedMethods()) // route: /
         app.use(BlogSchema.routes(), BlogSchema.allowedMethods()) // route: /blog
+        app.use(BlogReadSchema.routes(), BlogReadSchema.allowedMethods()) // route: /blog/read
     }
 }
---
+++ schema/blog_read_schema.ts
@@ -0,0 +1,49 @@
+import { applyGraphQL, gql } from "https://deno.land/x/oak_graphql/mod.ts";
+import axiod from "https://deno.land/x/axiod/mod.ts";
+
+const types = gql`
+  type ContentType {
+    text: String
+    img: String 
+  }
+
+  type TagType {
+      title: String
+      link: String
+      active: Boolean
+  }
+
+  type ReadType {
+    title: String
+    author: String
+    date_created: String
+    image_cover: String
+    image_content: String
+    content: [ContentType]
+    tags: [TagType]
+  }
+
+  type Query {
+    getData(id: String): ReadType!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getData: async function getRootData(parent: any, { id }: any) {
+      const url = `https://samehadaku-rest-api.herokuapp.com/blog/read/${id}`;
+      return await axiod.get(url).then((res) => {
+        console.log(res.data);
+        return res.data;
+      });
+    },
+  },
+};
+
+const GraphqlService = await applyGraphQL({
+  path: "/blog/read",
+  typeDefs: types,
+  resolvers,
+});
+
+export default GraphqlService;
